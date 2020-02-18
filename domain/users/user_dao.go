ade49d66c0fd9aa04be6d362a53b2183d89805ba
Mikhail Nilov <mail@mnilov.ru>
2020-02-18 20:48:20 +0700

    save

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -6,6 +6,12 @@ import (
 	"github.com/mnilov/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/mnilov/bookstore_users-api/utils/date_utils"
 	"github.com/mnilov/bookstore_users-api/utils/errors"
+	"strings"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
 var (
@@ -32,16 +38,28 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.DateCreated = date_utils.GetNowString()
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewInternalServerError(fmt.Sprintf("Email %s already exists!", user.Email))
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("Error when trying to save user: %s", err.Error()))
+	}
 
-	usersDB[user.Id] = user
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("Error when trying to get last insert ID: %s", err.Error()))
+	}
+
+	user.Id = userId
 	return nil
 }
